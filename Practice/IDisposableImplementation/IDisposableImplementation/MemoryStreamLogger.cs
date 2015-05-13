52cc468c6bdcd5a717627dbb12f1b471fb4ca4c7
iviu <iyura62@gmail.com>
2015-05-13 18:26:14 +0300

    Add IDisposableImplementation project

---
+++ Practice/IDisposableImplementation/IDisposableImplementation/MemoryStreamLogger.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace NetMentoring
+{
+    public class MemoryStreamLogger
+    {
+        private FileStream memoryStream;
+        private StreamWriter streamWriter;
+
+        public MemoryStreamLogger()
+        {
+            memoryStream = new FileStream(@"\log.txt", FileMode.OpenOrCreate);
+            streamWriter = new StreamWriter(memoryStream);
+        }
+
+        public void Log(string message)
+        {
+            streamWriter.Write(message);
+        }
+
+   }
+}
