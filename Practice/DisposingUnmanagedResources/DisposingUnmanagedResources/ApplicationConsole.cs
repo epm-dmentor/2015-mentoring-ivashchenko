b3aafa81ec6b0e95f40b19e9ef12ff18b8c45bcc
iviu <iyura62@gmail.com>
2015-05-14 12:47:03 +0300

    Add DisposingUnmanagedResource project

---
+++ Practice/DisposingUnmanagedResources/DisposingUnmanagedResources/ApplicationConsole.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Unmanaged
+{
+    public class ApplicationConsole
+    {
+        private const int StdOutputHandle = -11;
+        private readonly IntPtr _consoleHandle;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        private static extern bool WriteConsole(
+            IntPtr hConsoleOutput,
+            string lpBuffer,
+            uint nNumberOfCharsToWrite,
+            out uint lpNumberOfCharsWritten,
+            IntPtr lpReserved);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        public ApplicationConsole()
+        {
+            _consoleHandle = GetStdHandle(StdOutputHandle);
+
+            if (_consoleHandle == IntPtr.Zero)
+                throw new InvalidOperationException("Std handle is not available");
+
+            //WriteLine("Application console has been initialized");
+        }
+
+        public void WriteLine(string outputStr, params object[] args)
+        {
+            if (_consoleHandle == IntPtr.Zero)
+                throw new ObjectDisposedException("Object has been already disposed or was not allocated properly!");
+
+            var formatedStr = String.Format(outputStr, args);
+            uint charsWritten;
+            WriteConsole(_consoleHandle, formatedStr, (uint)formatedStr.Length, out charsWritten, IntPtr.Zero);
+            WriteConsole(_consoleHandle, "\n", 1, out charsWritten, IntPtr.Zero);
+        }
+    }
+}
---
+++ Practice/DisposingUnmanagedResources/Readme.md
@@ -0,0 +1,5 @@
+# Code example with unmanaged resources
+
+The task is to dispose resource safely. An example was given from MSDN: (SuprpressFinalize)[http://msdn.microsoft.com/en-us/library/system.gc.suppressfinalize(v=vs.110).aspx]
+
+The task is to free resource safely.
