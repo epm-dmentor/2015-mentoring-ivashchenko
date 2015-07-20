d8d51ecabd2f037129093d22d013217bae2a5537
iviu <iyura62@gmail.com>
2015-07-20 13:28:00 +0300

    List Implementation

---
+++ practice/tasks/ListIpl/ListIpl/LinkedList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListIpl
+{
+    public class Node<T>
+    {
+        private T _element;
+        private Node<T> _next;
+
+        public Node(T element)
+        {
+            this._element = element;
+        }
+
+        public T Value
+        {
+            get { return _element; }
+            set { _element = value; }
+        }
+
+        public Node<T> Next
+        {
+            get { return this._next; }
+            set { this._next = value; }
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private Node<T> _first;
+        private Node<T> _current;
+        private Node<T> _last;
+        private uint _size;        
+
+        public LinkedList()
+        {
+            _size = 0;
+            _first = _last = _current = null;
+        }
+
+        public bool isEmpty //проверка на пустоту
+        {
+            get
+            {
+                return _size == 0;
+            }
+        }
+
+        public void AddHead(T newElem)
+        {
+            Node<T> newNode = new Node<T>(newElem);
+
+            if (_first == null)
+            {
+                _first = _last = newNode;
+            }
+            else
+            {
+                newNode.Next = _first;
+                _first = newNode; 
+            }
+            _size++;
+        }
+
+        public void AddTail(T newElem)
+        {
+            Node<T> newNode = new Node<T>(newElem);
+
+            if (_first == null)
+                _first = _last = _current = newNode;
+            else
+            {
+                _last.Next = newNode;
+                _last = newNode;
+            }
+            _size++;
+        }
+
+        public void InsertAt(uint idx, T newElem)
+        {
+            if(idx > _size)
+                throw new ArgumentException("Out of index");
+
+            if(idx == 0)
+                AddHead(newElem);
+            else if(idx == _size  && _size > 2)
+                AddTail(newElem);
+            else
+            {
+                uint cc = 0;
+                _current = _first;
+                Node<T> newNode = new Node<T>(newElem);
+
+                for (uint i = 0; i < idx-1; i++)
+                {
+                    _current = _current.Next;
+                }
+
+                newNode.Next = _current.Next;
+                _current.Next = newNode;
+                _size++;
+            }
+        }
+
+        public void RemoveTail()
+        {
+            if(isEmpty)
+                return;
+
+            if (_size == 1)
+            {
+                _first = _last = _current = null;
+                _size = 0;
+                return;
+            }
+            _current = _first;
+
+            uint cc = 0;
+            while (cc != _size - 2)
+            {
+                _current = _current.Next;
+                cc++;
+            }
+            _current.Next = null;
+            _last = _current;
+            --_size;
+        }
+
+        public void RemoveHead()
+        {
+            if (isEmpty)
+                return;
+
+            _first = _first.Next;
+            _size--;
+
+        }
+
+        public void Print()
+        {
+            _current = _first;
+            uint cc = 0;
+            while ( cc < _size)
+            {
+                Console.WriteLine(_current.Value);
+                _current = _current.Next;
+                ++cc;
+            }
+        }
+    }
+}
---
+++ practice/tasks/ListIpl/ListIpl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListIpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> lst = new LinkedList<int>();
+            lst.AddHead(8);
+            lst.AddTail(7);
+            lst.AddTail(6);
+            lst.AddTail(5);
+            lst.AddTail(4);
+            lst.AddHead(9);
+//            lst.Print();
+//            Console.WriteLine("Insert new element");
+//            lst.InsertAt(2, 22);
+//            lst.Print();
+//            Console.WriteLine("Insert new element to head");
+            lst.InsertAt(3, 10);
+//            lst.Print();
+//            Console.WriteLine("Insert new element to tail");
+//            lst.InsertAt(7, 77);
+            lst.Print(); 
+
+            LinkedList<string> lstStr = new LinkedList<string>();
+            lstStr.AddTail("Tail");
+            lstStr.AddHead("Head");
+            lstStr.InsertAt(1,"Middle");
+            lstStr.Print();
+            Console.WriteLine("\nRemove tail node....\n");
+            lstStr.RemoveTail();
+            lstStr.Print();
+            Console.WriteLine("\nRemove head node....\n");
+            lstStr.RemoveHead();
+            lstStr.Print();
+        }
+    }
+}
